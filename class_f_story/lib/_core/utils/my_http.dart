5df79d6cfd25e0b212b9bd82f8edc2b76ebd94a5
DoyoungLee9720 <dlehdud224@gmail.com>
2025-02-03 14:02:00 +0900

    2025-02-03

--- class_f_story/lib/_core/utils/my_http.dart
+++ class_f_story/lib/_core/utils/my_http.dart
@@ -4,21 +4,20 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 // API 서버의 기본 URL 설정
 // 집 기본 API 주소
 // 그린 로컬 컴퓨터 기본 API 주소
-//전역 변수
+//전역 변수 사용
 final baseUrl = 'http://192.168.0.146:8080';
 
-//전역 변수
+//전역 변수 dio <--
 final dio = Dio(
   BaseOptions(
     baseUrl: baseUrl,
-    //요청 데이터 형식을 json 형식으로 지정하겠다.
-    contentType: 'application/json; charset=utf-8',
-    // 필수! dio는 200 상태코드 값이 아니면 무조건 오류로 바라 본다.
-    // true 설정 다른 상태 코드 값도 다 허용
+    contentType: 'application/json;charset=utf-8',
+    //주의 ! 200 번 이외에는  모두 에러로 간주 하게 기본값 설정되어 있음
+    //다른 상태코드 다 허용
     validateStatus: (status) => true,
   ),
 );
 
 // 중요 데이터 보관소(금고)
-// 민감한 데이터를 보관하는 안전한 금고 역할을 한다.
+// 로컬에 민감한 데이터를 보관하는 안전한 금고 역할을 한다.
 const secureStorage = FlutterSecureStorage();
--- class_f_story/lib/ui/pages/auth/join_page/join_page.dart
+++ class_f_story/lib/ui/pages/auth/join_page/join_page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/join_body.dart';
+
 class JoinPage extends StatelessWidget {
   const JoinPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: JoinBody(),
+    );
   }
 }
